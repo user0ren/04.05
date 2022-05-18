0859ca0654e593814a39cee22e4dfc9c5453d201
user0ren <88614567+user0ren@users.noreply.github.com>
2022-05-18 15:59:37 +0300

    Add files via upload

---
+++ 04 05/04 05/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Validating += textBox1_TextChanged;
+            textBox2.Validating += textBox2_TextChanged;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Выберите один из вариантов",
+                "Сообщение",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                errorProvider1.SetError(textBox1, "Не указано имя!");
+            }
+            else if (textBox1.Text.Length < 4)
+            {
+                errorProvider1.SetError(textBox1, "Слишком короткое имя!");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int age = 0;
+            if (String.IsNullOrEmpty(textBox2.Text))
+            {
+                errorProvider1.SetError(textBox2, "Не указан возраст!");
+            }
+            else if (!Int32.TryParse(textBox2.Text, out age))
+            {
+                errorProvider1.SetError(textBox2, "Некорретный возраст!");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+    }
+}
